ee42ad7169e6a104bada8f4e8158bdeeca118ebf
Dillon Steyl <dillon.steyl@snazzyshrimp.com>
2024-11-04 09:05:19 +1000

    add isort & black as default python formatters

--- init.lua
+++ init.lua
@@ -717,7 +717,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         gdscript = { 'gdformat' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
